d0358a29eefa9578a47cc632c85e57cad6ac3620
brayanmontoya1050 <161617317+brayanmontoya1050@users.noreply.github.com>
2024-09-16 16:37:12 -0600

    semana 7

---
+++ SEMANA VII/CONCATENACION/index.js
@@ -0,0 +1,30 @@
+const cadena1="Hola ";
+const cadena2="Mundo";
+const cadena3= cadena1 + cadena2
+console.log (cadena3);
+
+const ejemplo1 = 1;
+const ejemplo2 = "2"
+const ejemplo3 = ejemplo1 + ejemplo2
+console.log(ejemplo3);
+
+
+const nombre= "Brayan"
+const mensaje= " cuentanos,"
+const saludo=  " como estas"
+const imprimir = nombre + " " + mensaje + saludo
+const imprimirSaltoLinea = nombre + "\n" + mensaje + "\n"  +   saludo
+console.log (imprimir)
+console.log(imprimirSaltoLinea)
+
+
+const nombre2 = "Brayan"
+const mensaje2 = "cuentanos"
+const saludo2  = "como estas"
+const imprimir2 = `${nombre2} ${mensaje2} ${saludo2}`
+console.log (imprimir2)
+
+const nombre2 = "Brayan"
+const mensaje2 = "cuentanos"
+const saludo2  = "como estas"
+const imprimir2BackTick = $("nombre")
---
+++ SEMANA VII/SOLICITUD_DE_DATOS/index.js
@@ -0,0 +1,9 @@
+const nombre = prompt ("cual es tu nombre");
+console.log(nombre);
+console.log(typeof nombre)
+const edad = prompt ("cual es tu edad")
+console.log(edad);
+console.log(typeof edad);
+const edad2 = parsetInt(prompt("ingrese su edad")
+console.log(edad2)
+console.log(typeof edad2)v
